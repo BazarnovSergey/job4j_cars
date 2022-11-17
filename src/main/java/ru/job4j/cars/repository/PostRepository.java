13d447123e405701e578e4dcdf22823480061a1b
BazarnovSergey <ssergey101@yandex.ru>
2022-11-17 21:53:29 +0300

    1. Фильтры для объявлений машин [#4745 #361263]

--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.cars.model.Post;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -15,12 +16,20 @@ public class PostRepository {
 
     /**
      * Метод возвращает из базы данных все посты за последний день
+     * currentTime - текущее время
+     * currentTimeMinus24Hours - текущее время минус 24 часа
      *
      * @return коллекция List с постами
      */
     public List<Post> getPostsFromTheLastDay() {
+        var currentTime = LocalDateTime.now();
+        var currentTimeMinus24Hours = currentTime.minusHours(24);
         return crudRepository.query(
-                "select p from Post p where p.created > date_trunc('day', current_date )", Post.class);
+                "select p from Post p where p.created between "
+                        + ":fCurrentTime and :fCurrentTimeMinus24Hours", Post.class,
+                Map.of("fCurrentTime", currentTime,
+                        "fCurrentTimeMinus24Hours", currentTimeMinus24Hours)
+        );
     }
 
     /**
@@ -35,6 +44,7 @@ public class PostRepository {
 
     /**
      * Метод возвращает из базы данных посты с авто определенной марки
+     *
      * @param carBrand марка авто
      * @return коллекция List с постами
      */
